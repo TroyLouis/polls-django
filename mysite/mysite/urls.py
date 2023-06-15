48aef4ddbba5ad73f04b5049a84db69a033807e9
“Troy” <40181839+TroyLouis@users.noreply.github.com>
2023-06-15 16:35:22 -0700

    updated templating and added 404

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name ='home'),
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
 ]
